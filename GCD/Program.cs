acab3e290e48857fecfb1e5b05a0f82e7524e649
шама чич <shama_chich@icloud.com>
2019-02-24 20:52:38 +0300

    Do hust

--- GCD/Program.cs
+++ GCD/Program.cs
@@ -10,6 +10,40 @@ namespace GCD
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("INput a");
+            long a = (Convert.ToInt64(Console.ReadLine()));// ввод первого числа
+            Console.WriteLine("Input b");
+            long b = (Convert.ToInt64(Console.ReadLine()));// вввод второго числа
+
+            if (a < b) Exchg(ref a, ref b);//если a < b, то поменять значения переменных местами
+
+            Console.WriteLine();
+
+            Console.WriteLine("Result:");
+            Console.WriteLine(Gcd(a, b));
+        }
+
+        public static long Gcd(long num1, long num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("b не должен быть равен нулю");
+            }
+            while ((num1 % num2) != 0)//поиск НОД
+            {
+                Exchg(ref num1, ref num2);//меняем местами переменные
+                num2 = num2 % num1;//b присваиваем остаток от деления большего числа на меньшее
+            }
+
+            return num2;
+        }
+
+        public static void Exchg(ref long a1, ref long b1)
+        {
+            a1 = a1 * b1;// умножаем А на Бэ
+            b1 = a1 / b1;// делим A * B на B <=> (a * b) / b == a. То есть в b теперь значение a
+            a1 = a1 / b1;//делим (a * b) на a. то есть теперьв а значение b
         }
     }
+
 }
